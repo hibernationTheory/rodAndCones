c55fc90203c6f9fe52da6879b05352542d9d3bd5
Engin Arslan <hibernationtheory@gmail.com>
2015-02-08 22:47:21 -0500

    added linkData.py to dynamically create links in template

---
+++ rodAndCones/startpage/resource/linkData.py
@@ -0,0 +1,32 @@
+"""a module to store main page link information to be dynamically created in a template"""
+
+def main():
+	linkData = [
+		{'relativePath': 'startpage/images/temp/doom_01_thumb.jpg', 'description': 'doom_01'}, 
+		{'relativePath': 'startpage/images/temp/earlwillisbeal_01_thumb.jpg', 'description': 'earlwillisbeal_01'}, 
+		{'relativePath': 'startpage/images/temp/forrester_01_thumb.jpg', 'description': 'forrester_01'}, 
+		{'relativePath': 'startpage/images/temp/hands_01_thumb.jpg', 'description': 'hands_01'}, 
+		{'relativePath': 'startpage/images/temp/heads_01_thumb.jpg', 'description': 'heads_01'}, 
+		{'relativePath': 'startpage/images/temp/joyland_01_thumb.jpg', 'description': 'joyland_01'}, 
+		{'relativePath': 'startpage/images/temp/nehrudoom_01_thumb.jpg', 'description': 'nehrudoom_01'}, 
+		{'relativePath': 'startpage/images/temp/parker_01_thumb.jpg', 'description': 'parker_01'}, 
+		{'relativePath': 'startpage/images/temp/positronics_01_thumb.jpg', 'description': 'positronics_01'}, 
+		{'relativePath': 'startpage/images/temp/positronics_02_thumb.jpg', 'description': 'positronics_02'}, 
+		{'relativePath': 'startpage/images/temp/positronics_03_thumb.jpg', 'description': 'positronics_03'}, 
+		{'relativePath': 'startpage/images/temp/positronics_04_thumb.jpg', 'description': 'positronics_04'}, 
+		{'relativePath': 'startpage/images/temp/positronics_05_thumb.jpg', 'description': 'positronics_05'}, 
+		{'relativePath': 'startpage/images/temp/positronics_06_thumb.jpg', 'description': 'positronics_06'}, 
+		{'relativePath': 'startpage/images/temp/positronics_07_thumb.jpg', 'description': 'positronics_07'}, 
+		{'relativePath': 'startpage/images/temp/positronics_08_thumb.jpg', 'description': 'positronics_08'}, 
+		{'relativePath': 'startpage/images/temp/positronics_09_thumb.jpg', 'description': 'positronics_09'}, 
+		{'relativePath': 'startpage/images/temp/positronics_10_thumb.jpg', 'description': 'positronics_10'}, 
+		{'relativePath': 'startpage/images/temp/positronics_11_thumb.jpg', 'description': 'positronics_11'}, 
+		{'relativePath': 'startpage/images/temp/printedpages_01_thumb.jpg', 'description': 'printedpages_01'}, 
+		{'relativePath': 'startpage/images/temp/redmoon_01_thumb.jpg', 'description': 'redmoon_01'}, 
+		{'relativePath': 'startpage/images/temp/runaway_01_thumb.jpg', 'description': 'runaway_01'}, 
+		{'relativePath': 'startpage/images/temp/runners_01_thumb.jpg', 'description': 'runners_01'}, 
+		{'relativePath': 'startpage/images/temp/runners_02_thumb.jpg', 'description': 'runners_02'}, 
+		{'relativePath': 'startpage/images/temp/theyarecoming_01_thumb.jpg', 'description': 'theyarecoming_01'}, 
+		{'relativePath': 'startpage/images/temp/timescover_01_thumb.jpg', 'description': 'timescover_01'}
+	]
+	return linkData
