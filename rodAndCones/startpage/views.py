4c76ada4095dcd2ac19e6f7dfe69e81e7877fcf4
Engin Arslan <hibernationtheory@gmail.com>
2015-02-08 20:41:27 -0500

    a blank main.html with a corresponding view call

--- rodAndCones/startpage/views.py
+++ rodAndCones/startpage/views.py
@@ -1,6 +1,6 @@
-# Create your views here.
-
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 def main(request):
-  return HttpResponse("Hello World")
+  return render_to_response("startpage/main.html")
