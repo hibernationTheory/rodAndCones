33e1a491073e3d52dd64d40a31aec8a346ae16b9
Engin Arslan <hibernationtheory@gmail.com>
2015-02-08 22:48:04 -0500

    adjust main.html and views to dynamically create links from an external module

--- rodAndCones/startpage/views.py
+++ rodAndCones/startpage/views.py
@@ -1,6 +1,11 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from resource import linkData
 
 def main(request):
-  return render_to_response("startpage/main.html")
+	context = RequestContext(request)
+	linkDataAll = linkData.main()
+	context_dict = {"linkData":linkDataAll}
+
+	return render_to_response("startpage/main.html", context_dict)
