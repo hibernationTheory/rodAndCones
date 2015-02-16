c2f9d4765af263019fa8166c0bcbc02886069c1b
Engin Arslan <hibernationtheory@gmail.com>
2015-02-15 21:34:05 -0500

    further refine the layout function to support page sliding.

--- rodAndCones/static/projects/js/d3_initial.js
+++ rodAndCones/static/projects/js/d3_initial.js
@@ -1,5 +1,14 @@
-function createInterfaceForViz(name, title, subtitle, description) {
+function createInterfaceForViz(name, title, subtitle, description, index) {
     // creates the corresponding interface components for the visualization
+
+    var value = 0;
+    var negValue = 0;
+    if (index == 0) {
+        value = 0;
+    } else {
+        value = (index * 100) + "%"
+        negValue = "-" + value;
+    }
 
     // create the navigation text
     var span = $("<span></span>", {
@@ -26,10 +35,15 @@ function createInterfaceForViz(name, title, subtitle, description) {
     var section = $("<section></section>", {
         "class":"page " + "page-" + name,
     });
+
+    section.css("top", value);
+
     var header = $("<h2></h2>", {
         "class":"page-title",
         "text": title
     });
+    header.css("text-shadow", "1px 1px 0 #333");
+
     var infoSection = $("<h3></h3>", {
         "text":subtitle
     })
@@ -63,6 +77,7 @@ for (var i = 0; i < DATASET_SIZE; i++) {
     var yMultiplier = 50;
     var exampleID = "ex01"
     var exampleTitle = "Example 01"
+    var index = 0;
 
     var subtitle = "A basic D3 example for creating a Barchart"
     var description = "Using a random dataset to create a simple barchart. </br> Notes: </br> \
@@ -70,7 +85,7 @@ for (var i = 0; i < DATASET_SIZE; i++) {
     - at that point, you can apply margin: auto, </br>\
     - you need to set the x, y, width, height on the rect's inside svg. </br>\
     "
-    createInterfaceForViz(exampleID, exampleTitle, subtitle, description);
+    createInterfaceForViz(exampleID, exampleTitle, subtitle, description, index);
 
     // create the svg canvas
     d3.select(".page-" + exampleID).append("svg").attr({
@@ -99,7 +114,47 @@ for (var i = 0; i < DATASET_SIZE; i++) {
     });
 })();
 
+(function() {
+    var elemWidth = 20;
+    var padding = 0;
+    var yMultiplier = 50;
+    var exampleID = "ex02"
+    var exampleTitle = "Example 02"
+    var index = 1;
 
+    var subtitle = "Using d3 scales"
+    var description = ""
+    createInterfaceForViz(exampleID, exampleTitle, subtitle, description, index);
+
+    scaleX = d3.scale().linear().domain([0,1]).linear([0, SVG_WIDTH]);
+    scaleY = d3.scale().linear().domain([0,1]).linear([0, 1*yMultiplier]);
+
+    // create the svg canvas
+    d3.select(".page-" + exampleID).append("svg").attr({
+        "width":SVG_WIDTH,
+        "height":SVG_HEIGHT,
+        "class":"svg-" + exampleID
+    });
+
+    d3.select(".svg-" + exampleID)
+        .selectAll("rect")
+        .data(DATASET)
+        .enter()
+        .append("rect")
+        .attr({
+            "x":function(d,i) {
+                return i*elemWidth;
+                },
+            "y":function(d,i) {
+                return SVG_HEIGHT-d*yMultiplier;
+                },
+            "width":elemWidth,
+            "height": function(d, i) {
+                   return d*yMultiplier; 
+                },
+            "fill":"red"
+    });
+})();
 
 
 
