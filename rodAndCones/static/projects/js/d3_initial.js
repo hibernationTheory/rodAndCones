26e1fe74cf64c86cf08d5d1f71292368e84393d4
Engin Arslan <hibernationtheory@gmail.com>
2015-02-15 20:46:52 -0500

    create the layout more programmatically
    - adds a function to create the interface programmatically
    - simplifies the html

--- rodAndCones/static/projects/js/d3_initial.js
+++ rodAndCones/static/projects/js/d3_initial.js
@@ -7,11 +7,49 @@ for (var i = 0; i < dataSetSize; i++) {
     dataSet.push(num);
 }
 
+function createInterfaceForViz(name, title) {
+    // creates the corresponding interface components for the visualization
+
+    // create the navigation text
+    var span = $("<span></span>", {
+        "class":"targets",
+        "id":name
+    });
+    $(".wrapper").before(span);
+
+    // create the navigation links
+    var exampleLink = $("<a></a>", {
+                "href": "#" + name,
+                "text": name,
+                "class":"nav-link"
+            });
+
+    var navItem = $("<li></li>", {
+                "class":"nav-item"
+            });
+
+    $(".nav-list").append(navItem);
+    exampleLink.appendTo(navItem);
+
+    // create the pages
+    var section = $("<section></section>", {
+        "class":"page " + "page-" + name,
+    });
+    var header = $("<h2></h2>", {
+        "class":"page-title",
+        "text": title
+    });
+    $(".layout").append(section);
+    header.appendTo(section);
+};
+
 var svgWidth = 1000;
 var svgHeight = 300;
 var elemWidth = 20;
 var padding = 0;
 var yMultiplier = 50;
+
+createInterfaceForViz("ex01", "test");
 
 // create the svg canvas
 d3.select(".page-ex01").append("svg").attr({
