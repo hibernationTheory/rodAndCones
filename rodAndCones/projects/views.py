826379c4c479d85e195bbce553153562d12fe05b
Engin <hibernationtheory@gmail.com>
2015-03-12 21:56:05 -0400

    start templating in the html and corresponding view

--- rodAndCones/projects/views.py
+++ rodAndCones/projects/views.py
@@ -14,7 +14,17 @@ def d3_initial(request):
 
 def project_d3_playground(request):
 	#context = RequestContext(request)
-	return render_to_response("projects/project_d3_playground/main.html")
+	jsonDataPath = os.path.join(CURRENT_DIR, "projects", "project_d3_playground", "project_data.json")
+	with open(jsonDataPath) as jsonFile:
+		jsonData = json.load(jsonFile)
+
+	projectsData = []
+	for i in range(0, len(jsonData)):
+		currData = jsonData[str(i)]
+		projectsData.append(currData)
+
+	context_data = {"projects" :projectsData}
+	return render_to_response("projects/project_d3_playground/main.html", context_data)
 
 def project_portfolio(request):
 	jsonDataPath = os.path.join(CURRENT_DIR, "projects", "project_portfolio", "project_data.json")
