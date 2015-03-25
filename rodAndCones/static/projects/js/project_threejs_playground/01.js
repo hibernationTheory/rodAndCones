36448553e317cf96878d0c501644518210643c86
Engin Arslan <hibernationtheory@gmail.com>
2015-03-24 23:34:56 -0400

    fix the || null logic error, simplify the example file

--- rodAndCones/static/projects/js/project_threejs_playground/01.js
+++ rodAndCones/static/projects/js/project_threejs_playground/01.js
@@ -2,7 +2,11 @@ var myTHREEJS = {}
 myTHREEJS["renderer"] = createRenderer({"castShadow":true});
 myTHREEJS["scene"] = createScene();
 myTHREEJS["camera"] = createCamera();
+
 myTHREEJS["control"]  = new function () {
+  this.boxScaleX = 1;
+  this.boxScaleY = 0.1;
+  this.boxScaleZ = 100;
   this.rotationSpeedX = 0.001;
   this.rotationSpeedY = 0.001;
   this.rotationSpeedZ = 0.001;
@@ -11,6 +15,9 @@ myTHREEJS["control"]  = new function () {
 
 function addControls(controlObject) {
   var gui = new dat.GUI();
+  gui.add(controlObject, 'boxScaleX', 0.1, 100);
+  gui.add(controlObject, 'boxScaleY', 0.1, 100);
+  gui.add(controlObject, 'boxScaleZ', 0.1, 100);
   gui.add(controlObject, 'rotationSpeedX', -0.1, 0.1);
   gui.add(controlObject, 'rotationSpeedY', -0.1, 0.1);
   gui.add(controlObject, 'rotationSpeedZ', -0.1, 0.1);
@@ -22,16 +29,24 @@ function addControls(controlObject) {
   var camera = myTHREEJS["camera"];
   var renderer = myTHREEJS["renderer"];
   
-  var sphere =  createObject({"type":"sphere", "material":"lambert", "color":0xffffff, "name":"sphere", "size":[5,5,5], "parent":scene});
+  var plane =  createObject({"type":"plane", "material":"lambert", "color":0xcccccc, "name":"plane_ground", "size":[60,20], "parent":scene});
+  var sphere =  createObject({"type":"sphere", "material":"lambert", "color":0xffffff, "name":"sphere", "size":[4,20,20], "parent":scene});
   var cube = createObject({"type":"box", "material":"normal", "name":"cube", "parent":scene});
   var light = createObject({"type":"light_spot", "name":"light_spot_01", "color":0xffffff, "parent":scene, "castShadow":true});
-  var light_ambient = createObject({"type":"light_ambient", "name":"light_ambient_01", "color":0x0c0c0c, "parent":scene});
+  // var light_ambient = createObject({"type":"light_ambient", "name":"light_ambient_01", "color":0x0c0c0c, "parent":scene});
+
+  //plane.rotation.x=-0.5*Math.PI;
   
-  setTransform({"object":camera, "type":"t", "transform":[15,16,13]});
-  setTransform({"object":sphere, "type":"t", "transform":[-15,1,5]});
+  setTransform({"object":camera, "type":"t", "transform":[-30,40,30]});
+  setTransform({"object":plane, "type":"t", "transform":[15,0,0]});
+  setTransform({"object":plane, "type":"r", "transformX":-0.5*Math.PI});
+  setTransform({"object":cube, "type":"t", "transform":[-4,3,0]});
+  setTransform({"object":sphere, "type":"t", "transform":[20,4,2]});
+
   setTransform({"object":light, "type":"t", "transform":[40,60,-10]});
-  
   camera.lookAt(scene.position);
+
+  //createRandomObjects({"type":"box", "material":"lambert", "color":0xffffff, "name":"box-ground-plane", "size":[5,5,5], "parent":scene, "baseName":"cube", "sizeMultiplier":0.25})
   
   addControls(myTHREEJS["control"]);
 
@@ -46,14 +61,20 @@ function myRender() {
 
   myTHREEJS["renderer"].render(scene, myTHREEJS["camera"]);
 
-  var cube = scene.getObjectByName("cube");
-  var plane = scene.getObjectByName("plane");
+  //var cube = scene.getObjectByName("cube");
+  //var plane = scene.getObjectByName("plane");
+  var groundPlane = scene.getObjectByName("box-ground-plane");
 
-  cube.rotation.x += controller.rotationSpeedX;
-  cube.rotation.y += controller.rotationSpeedY;
-  cube.rotation.z += controller.rotationSpeedZ;
+  //cube.rotation.x += controller.rotationSpeedX;
+  //cube.rotation.y += controller.rotationSpeedY;
+  //cube.rotation.z += controller.rotationSpeedZ;
 
-  cube.scale.set(controller.scale, controller.scale, controller.scale);
+  /*
+  groundPlane.scale.x = controller.boxScaleX;
+  groundPlane.scale.y = controller.boxScaleY;
+  groundPlane.scale.z = controller.boxScaleZ;
+  */
+  //cube.scale.set(controller.scale, controller.scale, controller.scale);
   requestAnimationFrame(myRender);
 }
 
--- rodAndCones/static/projects/js/project_threejs_playground/common.js
+++ rodAndCones/static/projects/js/project_threejs_playground/common.js
@@ -1,3 +1,4 @@
+
 function createScene() {
   var scene = new THREE.Scene();
   return scene;
@@ -25,7 +26,7 @@ function createRenderer(data) {
 }
 
 function createRandomObjects(data) {
-  var amount = data && data["amount"] || 100;
+  var amount = data && data["amount"] || 500;
   var size = data && data["size"] || null;
   var sizeMultiplier = data && data["sizeMultiplier"] || 1;
   var baseName = data && data["baseName"] || null;
@@ -43,30 +44,32 @@ function createRandomObjects(data) {
   for (var i = 0; i < amount; i ++) {
     var randomNumber = Math.random();
     var randomNumberSizeScaled = randomNumber * sizeMultiplier;
-    var randomNumberScaled = randomNumber * 1;
+    var randomNumberScaled = randomNumber * 100;
     data["size"] = [sizeX * randomNumberSizeScaled, sizeY * randomNumberSizeScaled, sizeZ * randomNumberSizeScaled];
     data["name"] = data["baseName"] + "_" + i || "";
+    console.log(data["name"]);
     var object = createObject(data);
-    //setTransform({"object":object, "type":"t", "transform":[randomNumberScaled,randomNumberScaled,randomNumberScaled]});
+    if (Math.floor(randomNumberScaled) % 2 === 0) { randomNumberScaled *= -1}
+    setTransform({"object":object, "type":"t", "transform":[randomNumberScaled,randomNumberScaled,randomNumberScaled]});
   }
 }
 
 function createObject(data) {
-  var type = data && data["type"] || null;
+  var type = data && data["type"];
   var color = data && data["color"] || "0xcccccc"
   var isWireframe = data && data["wireframe"] || false;
   var material = data && data["material"] || "basic";
-  var name = data && data["name"] || null;
-  var parent = data && data["parent"] || null;
-  var size = data && data["size"] || null;
+  var name = data && data["name"];
+  var parent = data && data["parent"];
+  var size = data && data["size"];
   var castShadow = data && data["castShadow"] || false;
   var receiveShadow = data && data["receiveShadow"] || false;
   
-  if (type === null) {
+  if (type === undefined) {
     return null;
   }
   
-  if (size !== null) {
+  if (size !== undefined) {
     var sizeX = size[0];
     var sizeY = size[1];
     var sizeZ = size[2];
@@ -77,7 +80,7 @@ function createObject(data) {
   }
   
   if (type === "plane") {
-    var geometry = new THREE.PlaneGeometry(sizeX,sizeY,sizeZ,1);
+    var geometry = new THREE.PlaneGeometry(sizeX,sizeY);
   }
   if (type === "box") {
     var geometry = new THREE.BoxGeometry(sizeX,sizeY,sizeZ);
@@ -120,10 +123,10 @@ function createObject(data) {
       object.receiveShadow = true;
   }
   
-  if (name !== null) {
+  if (name !== undefined) {
     object.name = name;
   }
-  if (parent !== null) {
+  if (parent !== undefined) {
     parent.add(object);
   }
   
@@ -131,42 +134,42 @@ function createObject(data) {
 }
 
 function setTransform(data) {
-  var obj = data && data["object"] || null;
-  if (obj === null) { return null;}
+  var obj = data && data["object"];
+  if (obj === undefined) { return null;}
   
-  var transformType = data && data["type"] || null;
-  if (transformType === null) {return null;}
+  var transformType = data && data["type"];
+  if (transformType === undefined) {return null;}
   
-  var transform = data && data["transform"] || null;
-  var transformX = data && data["transformX"] || null;
-  var transformY = data && data["transformY"] || null;
-  var transformZ = data && data["transformZ"] || null;
+  var transform = data && data["transform"];
+  var transformX = data && data["transformX"];
+  var transformY = data && data["transformY"];
+  var transformZ = data && data["transformZ"];
   
-  if (transform !== null) {
-    var x = data && data["transform"][0] || null;
-    var y = data && data["transform"][1] || null;
-    var z = data && data["transform"][2] || null;
+  if (transform !== undefined) {
+    var x = data && data["transform"][0];
+    var y = data && data["transform"][1];
+    var z = data && data["transform"][2];
   }
   
  if (transformX) {
-    var x = data && data["transformX"] || null;
+    var x = data && data["transformX"];
   }
  if (transformY) {
-    var y = data && data["transformY"] || null;
+    var y = data && data["transformY"];
  }
  if (transformZ) {
-    var z = data && data["transformZ"] || null;
+    var z = data && data["transformZ"];
  }
   
   if (transformType === "t") {
-    if (x !== null) {obj.position.x = x;}
-    if (y !== null) {obj.position.y = y;}
-    if (z !== null) {obj.position.z = z;}
+    if (x !== undefined) {obj.position.x = x;}
+    if (y !== undefined) {obj.position.y = y;}
+    if (z !== undefined) {obj.position.z = z;}
   }
 
   if (transformType === "r") {
-    if (x !== null) {obj.rotation.x = x;}
-    if (y !== null) {obj.rotation.y = y;}
-    if (z !== null) {obj.rotation.z = z;}
+    if (x !== undefined) {obj.rotation.x = x;}
+    if (y !== undefined) {obj.rotation.y = y;}
+    if (z !== undefined) {obj.rotation.z = z;}
   }
 }
