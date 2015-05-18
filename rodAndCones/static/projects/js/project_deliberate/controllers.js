36896549575a2af617469c2b143bb1ab8236fc26
Engin Arslan <hibernationtheory@gmail.com>
2015-05-18 19:25:06 -0400

    switch to angular templating from django templating in 'deliberate'

--- rodAndCones/static/projects/js/project_deliberate/controllers.js
+++ rodAndCones/static/projects/js/project_deliberate/controllers.js
@@ -1,1 +1,7 @@
-angular.module("App");
+var App = angular.module('App');
+
+App.controller('TopicListCtrl', ['$scope', '$http', function($scope, $http) {
+	$http.get('../static/projects/js/project_deliberate/project_data.json').success(function(data) {
+		$scope.topics = data;
+	});
+}]);
