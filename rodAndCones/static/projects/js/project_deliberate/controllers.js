50f2d47923af02ebc9a4783d39c2a53e3c71f71f
Engin Arslan <hibernationtheory@gmail.com>
2015-05-18 20:53:18 -0400

    implement a new - hacky - way of working with project_data

--- rodAndCones/static/projects/js/project_deliberate/controllers.js
+++ rodAndCones/static/projects/js/project_deliberate/controllers.js
@@ -1,7 +1,7 @@
 var App = angular.module('App');
 
 App.controller('TopicListCtrl', ['$scope', '$http', function($scope, $http) {
-	$http.get('../static/projects/js/project_deliberate/project_data.json').success(function(data) {
+	$http.get('../static/projects/js/project_deliberate/topic_data/all_data.json').success(function(data) {
 		$scope.topics = data;
 	});
 }]);
