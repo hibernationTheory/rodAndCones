e0ae41b4959aebf7eb7e68ad40a528f6751d5819
Engin Arslan <enginarslan@Engin-Arslans-MacBook-Pro.local>
2015-06-01 22:06:12 -0400

    update populate script to reflect changes on the data model

--- rodAndCones/static/projects/js/project_deliberate/topic_data/populate_db.py
+++ rodAndCones/static/projects/js/project_deliberate/topic_data/populate_db.py
@@ -5,10 +5,10 @@ import os
 import sys
 
 CURRENT_DIR = os.path.dirname(__file__)
-JSON_PATH = os.path.join(CURRENT_DIR, 'all_data.json')
+JSON_CONTENT_DATA = os.path.join(CURRENT_DIR, 'data_workflowy.json')
+JSON_TECHNICAL_DATA = os.path.join(CURRENT_DIR, 'data_non_workflowy.json')
 BASE_PATH = os.path.abspath(os.path.join(CURRENT_DIR, os.pardir, os.pardir, os.pardir, os.pardir, os.pardir))
 DATABASE_PATH = os.path.join(BASE_PATH, 'database', 'database.db')
-#MODELS_PATH = os.path.join(BASE_PATH, 'projects')
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rodAndCones.settings')
@@ -16,18 +16,39 @@ sys.path.insert(0, BASE_PATH)
 from projects.models import Topic
 
 def main():
-	with open(JSON_PATH, 'r') as jsonFile:
-		jsonData = json.load(jsonFile)
+	with open(JSON_CONTENT_DATA, 'r') as jsonFile:
+		jsonContentData = json.load(jsonFile)
 
-	for data in jsonData:
-		topic = add_topic(name=data["name"],
-						  short_name=data["short_name"],
-						  tags=data["tags"],
-						  color=data["color"],
-						  image_name=data["image_name"],
-						  summary=data["summary"],
-						  categories=data["categories"],
-						  description=data["description"])
+	with open(JSON_TECHNICAL_DATA, 'r') as jsonFile:
+		jsonTechData = json.load(jsonFile)
+
+	for techData in jsonTechData:
+		jsonTopicData = None
+		for contentData in jsonContentData:
+			if contentData["name"].lower() == techData["short_name"].lower():
+				jsonTopicData = contentData
+		if not jsonTopicData:
+			break
+
+		topic = add_topic(name=techData["name"],
+						  short_name=techData["short_name"],
+						  tags=jsonTopicData["tags"],
+						  color=techData["color"],
+						  image_name=techData["image_name"],
+						  summary=jsonTopicData["summary"],
+						  categories=jsonTopicData["categories"],
+						  description=jsonTopicData["description"],
+						  competency = jsonTopicData["competency"],
+						  reason = jsonTopicData["reason"],
+						  rating = jsonTopicData["rating"],
+						  status = jsonTopicData["status"],
+						  plan = jsonTopicData["plan"],
+						  dependencies = jsonTopicData["dependencies"],
+						  related = jsonTopicData["related"],
+						  resources = jsonTopicData["resources"],
+						  links = jsonTopicData["links"]
+						  )
+
 	for t in Topic.objects.all():
 		print "{0}".format(str(t))
 
@@ -43,13 +64,14 @@ def add_topic(name, short_name, tags, color, image_name, summary, categories, de
 		categories=categories,
 		description=description,
 		competency=0,
-		reasoning="",
+		reason="",
 		rating=0,
 		status="",
 		priority=0,
 		dependencies="",
 		related="",
-		resources=""
+		resources="",
+		links = ""
 		)[0]
 	return t
 
