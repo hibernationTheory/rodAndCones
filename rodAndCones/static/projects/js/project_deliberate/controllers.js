b13b6d4795b134e2d05de2db26285278cabc0ccc
Engin Arslan <enginarslan@Engin-Arslans-MacBook-Pro.local>
2015-06-18 23:36:58 -0400

    adds angular controller and directives

--- rodAndCones/static/projects/js/project_deliberate/controllers.js
+++ rodAndCones/static/projects/js/project_deliberate/controllers.js
@@ -10,15 +10,42 @@ App.directive('changeColor', function() {
   }
 });
 
-App.directive('setTetrad', function() {
+App.directive('complementColor', function() {
   return {
     link:function($scope, element, attrs) {
       $scope.$evalAsync(function() {
-        setTetrad(element);
+        complementColor(element);
       })
     }
   }
 });
+
+App.directive('brightenColor', function() {
+  return {
+    link:function($scope, element, attrs) {
+      $scope.$evalAsync(function() {
+        brightenColor(element);
+      })
+    }
+  }
+});
+
+App.directive('shortenText', function() {
+  return {
+    link:function($scope, element, attrs) {
+      $scope.$evalAsync(function() {
+        shortenText(element);
+      })
+    }
+  }
+});
+
+App.controller('searchFilter', function($scope, $element) {
+  var elementCategory = $element.data('item-category');
+  console.log(elementCategory);
+  this.category = elementCategory;
+  $scope.showElement = showElement;
+})
 
 var changeColor = function(element) {
   var el = element[0]
@@ -31,25 +58,113 @@ var changeColor = function(element) {
   }
 };
 
-var setTetrad = function(element) {
+var showElement = function(query, selection, id) {
+  var element = document.getElementById(id);
+  var el_category = element.dataset.itemCategory;
+  var el_name = element.dataset.itemName;
+  var el_name = el_name.toLowerCase();
+
+  var match_selection;
+  var match_query;
+
+  if (!query && !selection) {
+    return true;
+  }
+
+  if (selection) {
+    if (selection.toLowerCase().trim() === el_category.toLowerCase().trim()) {
+      match_selection = true;
+    } else {
+      match_selection = false;
+    }
+  } else {
+    match_selection = true;
+  }
+
+  if (query) {
+    var query = query.toLowerCase();
+    if (el_name.indexOf(query) !== -1) {
+        match_query = true;
+    } else {
+      match_query = false;
+    }
+  } else {
+    match_query = true;
+  }
+
+  if ( match_query && match_selection ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+App.controller('itemContainer', function($scope, $element) {
+  // not using this for now, it displays the child count not at the same things get hidden. 
+  // (but one item later)
+  var childrenCount = $element.get(0).childElementCount;
+
+  $scope.$watch(function() {
+    var children = $element.get(0).childNodes;
+    var topicItems = 0;
+    var hiddenItems = 0;
+
+    for (var i = 0; i < children.length; i++) {
+      var node = children[i];
+      if (node.className && node.className.indexOf("topic-item") !== -1) {
+        topicItems += 1;
+        if (node.className.indexOf("ng-hide") !== -1 ) {
+          hiddenItems += 1;
+        }
+      }
+    }
+    if (topicItems == hiddenItems) {
+      console.log('allHidden');
+    }
+  });
+});
+
+
+var complementColor = function(element) {
   var el = element[0]
   var color = el.getAttribute('data-color');
-  var colors = tinycolor(color).tetrad();
-  var complementery = complement(color).toHexString()
-  //var tetrad = colors.map(function(t) { return t.toHexString(); });
+  var complementery = tinycolor(color).complement().toHexString();
   el.style.backgroundColor = complementery;
 }
 
-function complement(color) {
-    var hsl = tinycolor(color).toHsl();
-    hsl.h = (hsl.h + 180) % 360;
-    return tinycolor(hsl);
+var brightenColor = function(element) {
+  var el = element[0]
+  var color = el.getAttribute('data-color');
+
+  var brightness = tinycolor(color).getBrightness();
+  if (brightness <= 96) {
+    var brightenAmount = 20;
+  } else {
+    var brightenAmount = 20;
+  }
+
+  var colors = tinycolor(color).brighten(brightenAmount).toHexString()
+  el.style.backgroundColor = colors;
+  changeColor(element);
+}
+
+var shortenText = function(element) {
+  var el = element[0]
+  var text = el.innerText;
+  if (text) {
+    var words = text.split(" ");
+    var newText = words.splice(0, 30).join(" ");
+    newText = newText + " [...]";
+  } else {
+    return null;
+  }
+  el.innerText = newText; 
 }
 
 function createChart(hostSelector, givenData) {
 	/* this is working fine but needs to work in a way that it would clear the previous graph when updated */
 	console.log(hostSelector, givenData)
-	/*
+	
 	var w = 150;
 	var h = 150;
 	console.log(w, h);
@@ -81,6 +196,6 @@ function createChart(hostSelector, givenData) {
     arcs.append("path")
         .attr('d', arc)
         .attr('fill', function(d){ return color(d.data);})
-    */
+    
 }
 
